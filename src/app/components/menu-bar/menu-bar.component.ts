b7f03096386d019a63ccc239b7eabbc5d4223d4d
rcsilva96 <rsilv1822@gmail.com>
2023-10-09 23:02:17 -0300

    components dynamic properties created

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  styleUrls: ['./menu-bar.component.css', './menu-bar.responsive.component.css'] // Pode colocar mais de um CSS, nesse caso é para agregar responsividade.
 })
 export class MenuBarComponent {
 
