658e4ce5778ee4dcd710421471cc21d1d8179d61
rcsilva96 <rsilv1822@gmail.com>
2023-10-11 22:41:34 -0300

    Added more news links

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -31,4 +31,28 @@ export const dataFake = [
     "description": "Vega foi capaz de abrir portas sozinha e cantar o hino da Coreia",
     "photoCover": "https://cdn.pixabay.com/photo/2020/09/04/10/09/cat-5543580_960_720.jpg"
   },
+  {
+    "Id": "5",
+    "title": "Aprenda a jogar com Little Mac no Super Smash Bros",
+    "photoCover": "https://qph.cf2.quoracdn.net/main-qimg-74944b445df5903679a052008d2a2f29-lq",
+    "description": "Rápido e forte, Little Mac é uma ótima escolha de personagem para seu jogo."
+  },
+  {
+    "Id": "6",
+    "title": "Gatos programadores prometem deixar o ambiente mais leve",
+    "photoCover": "https://img.freepik.com/fotos-premium/gato-fofo-gerado-por-ia-como-um-programador-de-jogos-com-fones-de-ouvido-em-sessao-de-codificacao-noturna_961443-65.jpg",
+    "description": "Meu gato está sempre comigo e decidi ensinar ele a programar, quando me dei conta, ele havia sido contratado como Dev full-stack - afirma um leitor do blog",
+  },
+  {
+    "Id": "7",
+    "title": "A miniatura que faltava na sua coleção: Ford Fiesta Roxo!",
+    "photoCover": "https://http2.mlstatic.com/D_NQ_NP_922015-MLB31993479912_082019-O.webp",
+    "description": "Clássico da Ford já está disponível para compra em todo o país!",
+  },
+  {
+    "Id": "8",
+    "title": "SPFC conquista título inédito da Copa do Brail",
+    "photoCover": "https://pbs.twimg.com/media/F4opEb8WcAAGYg0.jpg:large",
+    "description": "Vega foi capaz de abrir portas sozinha e cantar o hino da Coreia"
+  },
 ]
